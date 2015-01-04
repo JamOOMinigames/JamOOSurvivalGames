d69ad3c9cd1eca77611cbd13beb4297a5e9d8ed4
Mitchellbrine <awsomeman500@gmail.com>
2015-01-03 19:16:03 -0800

    Initial Commit

---
+++ src/main/java/com/jamoorev/kroostylGames/Main.java
@@ -0,0 +1,19 @@
+package com.jamoorev.kroostylGames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	public void onLoad() {
+		
+	}
+	
+}
