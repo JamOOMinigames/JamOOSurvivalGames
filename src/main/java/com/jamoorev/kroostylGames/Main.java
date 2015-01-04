a06f07bf4a599b9bc96b504e32045340b4fb4ac3
Mitchellbrine <awsomeman500@gmail.com>
2015-01-03 19:41:28 -0800

    Added game state

--- src/main/java/com/jamoorev/kroostylGames/Main.java
+++ src/main/java/com/jamoorev/kroostylGames/Main.java
@@ -1,11 +1,20 @@
 package com.jamoorev.kroostylGames;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
+	
+	public static Main instance;
+	
+	public static int gameState;
+	
+	public Main() {
+		instance = this;
+	}
+	
 	public void onEnable() {
-		
+		gameState = 0;
 	}
 	
 	public void onDisable() {
